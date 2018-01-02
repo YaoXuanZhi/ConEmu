cd6273b80de72168e6f0ce11efeed36429aa8127
yaoxuanzhi <yaoxuanzhi@outlook.com>
2018-01-02 19:49:16 +0800

    修复clone terminals仓库指令错误

--- src/modules/clonemodules.bat
+++ src/modules/clonemodules.bat
@@ -1,3 +1,3 @@
 git clone https://github.com/Maximus5/minhook.git
-git clone terminals https://github.com/Maximus5/cygwin-connector.git
+git clone https://github.com/Maximus5/cygwin-connector.git terminals
 git clone https://github.com/Maximus5/json-parser.git
